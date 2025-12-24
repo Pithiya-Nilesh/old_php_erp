68cc5903c0c3cc473b01c574cc28ddbdb83375a6
Nilesh-Pithiya-Sanskar <nilesh@sanskartechnolab.com>
2025-12-24 16:35:56 +0530

    init commit with migrate data

---
+++ old_php_erp/old_php_erp/doctype/old_erp_vidhyapeeth_revision_test/old_erp_vidhyapeeth_revision_test.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Sanskar Technolab and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Old ERP Vidhyapeeth Revision Test", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ old_php_erp/old_php_erp/doctype/old_erp_vonline_exam_ans_june_19/old_erp_vonline_exam_ans_june_19.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Sanskar Technolab and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Old ERP Vonline Exam Ans June 19", {
+// 	refresh(frm) {
+
+// 	},
+// });
